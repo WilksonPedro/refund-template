41dbd8bf5559b42fc6ffbc1e551a572745278a06
wilkson pedro da Silva <wil.thydus@gmail.com>
2024-11-11 17:56:47 -0300

    feat: atualização do formulário para incluir categoria oculta e adicionando evento de escuta

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const amount = document.getElementById("amount");
 const expense = document.querySelector("#expense");
 const category = document.querySelector("#category");
 
-
+// Formatando o valor do input para moeda
 amount.oninput = () => {
     let value = amount.value.replace(/\D/g, "");
     // Aplicando a formatação do value para numero e junto para centavos
@@ -11,6 +11,7 @@ amount.oninput = () => {
     amount.value = formatCurrencyBrl(value);
 }
 
+// Função auxiliar para formatar o valor para moeda
 function formatCurrencyBrl(value) {
     value = value.toLocaleString("pt-BR", {
         style: "currency",
@@ -20,6 +21,18 @@ function formatCurrencyBrl(value) {
     return value 
 }
 
+// Capturando o evento de submit no formulário
 form.onsubmit = (event) =>  {
     event.preventDefault();
+    // Criando um objeto dentro do evento com os detalhes do formulario
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+    
 }
+
